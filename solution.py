d2dae530039a2eafd88749ceca320c580691b8b5
bengü su akay <90608475+Suakay@users.noreply.github.com>
2021-09-18 18:01:25 +0300

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,11 @@
+#Time Complexity: O(N)
+#Space Complexity: O(1)
+
+def solution(A):
+    N = len(A) + 1
+    missing = ((N + 1) * N) / 2
+    
+    for x in A:
+        missing -= x
+    
+    return missing
